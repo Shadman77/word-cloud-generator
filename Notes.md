bf3bdc1198c5455504cc6e3418eae31ca29734a8
Shadman Saif <shadmansaif@outlook.com>
2023-12-14 09:56:51 +0600

    notes

---
+++ Notes.md
@@ -0,0 +1,2 @@
+# Git pre hooks
+* To make the pre hooks, run "make git-hooks". This will run tests before git commit and will only allow the commit to happen if the tests pass.
--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 )
 
 // serves up our static content like html
+//
 //go:embed static/*
 var staticFiles embed.FS
 
@@ -69,6 +70,10 @@ func main() {
 	staticFs := http.FileServer(getStaticFiles())
 
 	mux := http.NewServeMux()
+
+	// Uncomment to make test fail
+	// the_answer := 42
+	// fmt.Printf("The answer is %s\n", the_answer)
 
 	// routes
 	mux.HandleFunc("/api", receiveJSONHandler)
